c7296159cddbf2fee823ac114e45e44cb8058db4
matttully1 <matttully1@gmail.com>
2015-11-17 16:05:32 -0500

    The two badlogic sprites collide
    fixtureDef.filter.maskBits = WORLD_ENTITY | PHYSICS_ENTITY;
    fixtureDef2.filter.maskBits = WORLD_ENTITY | PHYSICS_ENTITY;

---
+++ android/src/com/s0869223/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.s0869223.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.s0869223.Physics4;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Physics4(), config);
+	}
+}
---
+++ desktop/src/com/s0869223/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.s0869223.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.s0869223.Physics4;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Physics4(), config);
+	}
+}
